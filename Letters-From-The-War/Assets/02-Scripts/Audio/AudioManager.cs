8a280d9af2ec719c7551d8d4cd8fdf29806cac91
Mike <mic.bellotti02@gmail.com>
2024-10-01 13:43:05 +0200

    add new background music

--- Letters-From-The-War/Assets/02-Scripts/Audio/AudioManager.cs
+++ Letters-From-The-War/Assets/02-Scripts/Audio/AudioManager.cs
@@ -3,17 +3,13 @@ using UnityEngine;
 
 public class AudioManager : Singleton<AudioManager>
 {
-    [Header("Background Music")]
-    public AudioSource _loopAudioSource;
-    [SerializeField] [ProgressBar(0,100, 1, 0, 0)] private int _musicVolume = 50;
+    [Header("Background Music")] public AudioSource _loopAudioSource;
 
-    [Header("SFX")]
-    public AudioSource _oneShotAudioSource;
-    [SerializeField] [ProgressBar(0,100, 1, 0, 0)] private int _sfxVolume = 100;
+    [SerializeField] [ProgressBar(0, 100, 1, 0, 0)]
+    private int _musicVolume = 50;
 
-    private void Start()
-    {
-        _oneShotAudioSource.volume = (float)_sfxVolume / 100;
-        _loopAudioSource.volume = (float)_musicVolume / 100;
-    }
+    [Header("SFX")] public AudioSource _oneShotAudioSource;
+
+    [SerializeField] [ProgressBar(0, 100, 1, 0, 0)]
+    private int _sfxVolume = 100;
 }
