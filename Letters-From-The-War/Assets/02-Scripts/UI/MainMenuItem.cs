b095ffe851d81b0364c5cd5db3517304e04682df
Mike <mic.bellotti02@gmail.com>
2024-10-02 16:09:26 +0200

    update main menu

--- Letters-From-The-War/Assets/02-Scripts/UI/MainMenuItem.cs
+++ Letters-From-The-War/Assets/02-Scripts/UI/MainMenuItem.cs
@@ -6,29 +6,20 @@ using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuItem : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler, IPointerDownHandler
+public class MainMenuItem : MonoBehaviour, IPointerEnterHandler
 {
     #region FIELDS
-    private RectTransform _iconTransform;
-    private TMP_Text _text;
     private AudioManager _audioManager;
-    private const int _HIGHLITED_ICON_ROTATION = 45;
+    [Header("Flags")]
+    [SerializeField] private bool _isEnabled = true;
     
-    [Header("Image Parameters")]
-    [SerializeField] private Color _baseTextColor;
-    [SerializeField] private Color _highlitedTextColor;
-    [SerializeField] private Color _clickedTextColor;
     [Header("SFX Parametes")]
     [SerializeField] private AudioClip _sfx;
     [SerializeField] [ProgressBar(0,100, 1f, 0f, 0f)]private int _sfxVolume = 100;
+    
     #endregion
 
     #region UNITY_CALLS
-    private void Awake()
-    {
-        _iconTransform = GetComponentInChildren<Image>().rectTransform;
-        _text = GetComponentInChildren<TMP_Text>();
-    }
 
     private void Start()
     {
@@ -48,29 +39,16 @@ public class MainMenuItem : MonoBehaviour, IPointerEnterHandler,IPointerExitHand
     }
     
     #region Pointer Events Management
-    public void OnPointerEnter(PointerEventData eventData) => ToggleHighlited(true);
-    
-    public void OnPointerExit(PointerEventData eventData) => ToggleHighlited(false);
+    public void OnPointerEnter(PointerEventData eventData) => ToggleHighlited();
 
-    public void OnPointerDown(PointerEventData eventData) => ToggleClicked(true);
-  
-    private void ToggleHighlited(bool toggle)
+    private void ToggleHighlited()
     {
-        if (toggle)
+        if (_isEnabled)
         {
-            _iconTransform.Rotate(0,0,_HIGHLITED_ICON_ROTATION);
-            _text.color = _highlitedTextColor;
             _audioManager._oneShotAudioSource.PlayOneShot(_sfx);
-        }
-        else
-        {
-            _iconTransform.Rotate(0,0,-_HIGHLITED_ICON_ROTATION);
-            _text.color = _baseTextColor;
         }
     }
 
-    //forma if contratta usando operatore ternario
-    private void ToggleClicked(bool toggle) => _text.color = toggle ? _clickedTextColor : _baseTextColor;
     #endregion
     #endregion
 }
