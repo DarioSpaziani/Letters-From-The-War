357135e464f4e561808f6508394190a72899e728
Mike <mic.bellotti02@gmail.com>
2024-11-11 14:04:16 +0100

    Feat: add text intro

--- Letters-From-The-War/Assets/02-Scripts/UI/IntroImages.cs
+++ Letters-From-The-War/Assets/02-Scripts/UI/IntroImages.cs
@@ -1,36 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class IntroImages : MonoBehaviour
 {
+    #region FIELDS
+
     [Header("Images")] 
-    [SerializeField] private List<Sprite> _imagesList;
+    [SerializeField] private List<Slide> _imagesList;
     [SerializeField] private float _showTime = 3f;
-    [SerializeField] private float _intervalTime = 0.5f;
+    [FormerlySerializedAs("_intervalTime")] [SerializeField] private float _fadeTime = 0.5f;
     [SerializeField] private Image _showedImage;
+    [SerializeField] private TMP_Text _showedText;
     [Header("Game Object References")]
     [SerializeField] private Fade _fade;
     [SerializeField] private Intro _canvasIntro;
 
+    #endregion
+
+    #region UNITY CALLS
+
     private void Start()
     {
-        _showedImage.sprite = _imagesList[0];
+        _showedImage.sprite = _imagesList[0].image;
+        _showedText.text = _imagesList[0].text;
         StartCoroutine(SlideImages());
     }
 
     private IEnumerator SlideImages()
     {
-        foreach (Sprite sprite in _imagesList)
+        foreach (Slide slide in _imagesList)
         {
-            yield return new WaitForSeconds(_intervalTime+0.2f);
-            _showedImage.sprite = sprite;
-            _showedImage.CrossFadeColor(Color.white, _intervalTime, false, false);
+            yield return new WaitForSeconds(_fadeTime+0.2f);
+            _showedImage.sprite = slide.image;
+            _showedText.text = slide.text;
+            _showedImage.CrossFadeColor(Color.white, _fadeTime, false, false);
+            _showedText.CrossFadeColor(Color.white, _fadeTime, false, false);
             yield return new WaitForSeconds(_showTime);
-            _showedImage.CrossFadeColor(Color.black, _intervalTime+0.2f, false, false);
-            yield return new WaitForSeconds(_intervalTime);
+            _showedImage.CrossFadeColor(Color.black, _fadeTime+0.2f, false, false);
+            _showedText.CrossFadeColor(Color.black, _fadeTime+0.2f, false, false);
+            yield return new WaitForSeconds(_fadeTime);
         }
         _canvasIntro.SkipIntro();
     }
+    
+    [Serializable]
+    private struct Slide
+    {
+        public Sprite image;
+        public string text;
+    }
+
+    #endregion
 }
